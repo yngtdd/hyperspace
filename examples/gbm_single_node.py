772ec26a2e33fad82ffd8c3aae7334d87108e039
Todd <myoung44@vols.utk.edu>
2017-12-14 15:27:16 -0500

    :zap:

--- examples/gbm_single_node.py
+++ examples/gbm_single_node.py
@@ -1,20 +1,9 @@
-"""
-Gradient Boosting Regressor
-A hyperspace distributed version of Scikit-Optimize's hyperparameter optimization example
-
-To Run:
-mpirun -n 32 python gbm_regressor.py --results_dir <full_path_to.../gbm_results>
-
-Note: we use 32 processes in this example (hence -n 32 above) since we have 2**5
-combinations of hyperparameter subspaces.
-"""
 from sklearn.datasets import load_boston
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import cross_val_score
+from skopt import gp_minimize
 import numpy as np
 import argparse
-
-from hyperspace import hyperdrive
 
 
 boston = load_boston()
@@ -23,7 +12,7 @@ n_features = X.shape[1]
 
 reg = GradientBoostingRegressor(n_estimators=50, random_state=0)
 
-
+@profile
 def objective(params):
     """
     Objective function to be minimized.
@@ -35,11 +24,10 @@ def objective(params):
         - Controlled by hyperspaces's hyperdrive function.
         - Order preserved from list passed to hyperdrive's hyperparameters argument.
     """
-    #max_depth, learning_rate, max_features, min_samples_split, min_samples_leaf = params
-    max_depth, max_features, min_samples_split, min_samples_leaf = params
+    max_depth, learning_rate, max_features, min_samples_split, min_samples_leaf = params
 
     reg.set_params(max_depth=max_depth,
-                   #learning_rate=learning_rate,
+                   learning_rate=learning_rate,
                    max_features=max_features,
                    min_samples_split=min_samples_split,
                    min_samples_leaf=min_samples_leaf)
@@ -47,25 +35,19 @@ def objective(params):
     return -np.mean(cross_val_score(reg, X, y, cv=5, n_jobs=-1,
                                     scoring="neg_mean_absolute_error"))
 
-
+@profile
 def main():
     parser = argparse.ArgumentParser(description='Setup experiment.')
     parser.add_argument('--results_dir', type=str, help='Path to results directory.')
     args = parser.parse_args()
 
     hparams = [(2, 10),             # max_depth
-               #(10.0**-2, 10.0**0), # learning_rate
+               (10.0**-2, 10.0**0), # learning_rate
                (1, 10),             # max_features
                (2, 100),            # min_samples_split
                (1, 100)]            # min_samples_leaf
 
-    hyperdrive(objective=objective,
-               hyperparameters=hparams,
-               results_path=args.results_dir,
-               model="GP",
-               n_iterations=15,
-               verbose=True,
-               random_state=0)
+    res_gp = gp_minimize(objective, hparams, n_calls=50, random_state=0)
 
 
 if __name__ == '__main__':
