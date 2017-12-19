06d4d4d6583a53b4bfef0ad138408fca969a07e4
Todd <myoung44@vols.utk.edu>
2017-12-19 14:50:14 -0500

    Starting wheel branch

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
+# from os import path
+
+# here = path.abspath(path.dirname(__file__))
+#
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
 
 setup(
-    name = 'hyperspace',
-    packages = ['hyperspace'],
+    name='hyperspace',
+    version="0.2.0",
+    packages=find_packages(),
     install_requires=['numpy', 'scikit-optimize', 'scikit-learn'],
-    version = '0.2.0',
-    description = 'Distributed Bayesian model-based optimization with Scikit-Optimize',
-    author = 'Todd Young',
-    author_email = 'youngmt1@ornl.gov',
-    url = 'https://code-int.ornl.gov/ygx/hyperspace',
-    keywords = ['parallel', 'optimization', 'mbo'],
+
+    # metadata for upload to PyPI
+    author="Todd Young",
+    author_email="youngmt1@ornl.gov",
+    description="Distributed Bayesian model-based optimization with Scikit-Optimize",
+    license="MIT",
+    keywords="parallel optimization smbo",
+    url="https://github.com/yngtodd/hyperspace",
 )
