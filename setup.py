685befaa4582edf2c6b35d23128f3a59fc23d23e
Todd <myoung44@vols.utk.edu>
2018-01-13 15:03:35 -0500

    Add missing dependence on mpi4py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='hyperspace',
     version="0.2.0",
     packages=find_packages(),
-    install_requires=['numpy', 'scikit-optimize', 'scikit-learn'],
+    install_requires=['numpy', 'scikit-optimize', 'scikit-learn', 'mpi4py'],
 
     # metadata for upload to PyPI
     author="Todd Young",
