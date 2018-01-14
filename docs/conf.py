c792371b3a5ea7f7563edf36eae02ccdc5f07d70
Todd <myoung44@vols.utk.edu>
2018-01-14 01:09:49 -0500

    templates, maybe?

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@
 extensions = ['sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
