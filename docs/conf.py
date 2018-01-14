b39eb3536b844bb43ffa328945b5400356e2ef28
Todd <myoung44@vols.utk.edu>
2018-01-14 00:54:30 -0500

    remove fonts from pytorch css

--- docs/conf.py
+++ docs/conf.py
@@ -106,7 +106,6 @@ html_static_path = ['_static']
 # html_style_path = 'css/pytorch_theme.css'
 html_context = {
     'css_files': [
-        'https://fonts.googleapis.com/css?family=Lato',
         '_static/css/pytorch_theme.css'
     ],
 }
