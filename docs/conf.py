a6b69f1c6b063e7f6a42b92e13e2b44a14cb7c8e
Todd <myoung44@vols.utk.edu>
2018-01-14 00:21:24 -0500

    Adjusting logo

--- docs/conf.py
+++ docs/conf.py
@@ -89,7 +89,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'display_version': True,
+    'logo_only': True,
+}
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
