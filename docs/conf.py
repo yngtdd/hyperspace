dee944d9a642b0d6e976bf1686f31c9d7ed46134
Todd <myoung44@vols.utk.edu>
2018-01-14 01:54:01 -0500

    I will do this.

--- docs/conf.py
+++ docs/conf.py
@@ -112,7 +112,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = "_static/img/hyperspace_logo.png"
+html_logo = "_static/img/hyperspace_logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
