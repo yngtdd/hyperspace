8b83813d700ac04ffeec2433c36fe2da048cc237
Todd <myoung44@vols.utk.edu>
2018-01-14 00:38:04 -0500

    static now visible

--- docs/conf.py
+++ docs/conf.py
@@ -96,12 +96,12 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "./.static/img/hyperspace_logo.png"
+html_logo = "_static/img/hyperspace_logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
