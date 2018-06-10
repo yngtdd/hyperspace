758b198a3de96dd815dc03e1b3521e5b1f8c62b3
Todd <myoung44@vols.utk.edu>
2018-06-10 14:08:17 -0400

    Starting robo branch

---
+++ hyperspace/space/robo/__init__.py
@@ -0,0 +1,12 @@
+from .space import HyperInteger
+from .space import HyperReal
+from .mapping_space import create_hyperspace
+from .mapping_space import create_hyperbounds
+
+
+__all__ = (
+    "create_hyperbounds"
+    "create_hyperspace",
+    "HyperInteger",
+    "HyperReal"
+)
