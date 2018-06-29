431dfec5fc89fd1a07e10a550b95050a14571ca1
Todd Young <young.todd.mk@gmail.com>
2018-06-29 10:38:54 -0400

    :fire:

--- hyperspace/space/robo/__init__.py
+++ hyperspace/space/robo/__init__.py
@@ -1,9 +1,11 @@
 from .space import RoboInteger
 from .space import RoboReal
 from .mapping_space import create_robospace
+from .mapping_space import convert_robospace
 
 
 __all__ = (
+    "conver_robospace",         
     "create_robospace",
     "RoboInteger",
     "RoboReal"
