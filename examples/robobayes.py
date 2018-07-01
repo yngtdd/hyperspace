08264f75d19a716af3ccad1dbc432cf424ddded1
Todd <myoung44@vols.utk.edu>
2018-07-01 00:53:27 -0400

    Adding Robo Bayes

---
+++ examples/robobayes.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from hyperspace.benchmarks import StyblinksiTang 
+from hyperspace.hyperdrive.robo.hyperbayes import hyperdrive
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Setup experiment.')
+    parser.add_argument('--results_dir', type=str, help='Path to results directory.')
+    args = parser.parse_args()
+
+    stybtang = StyblinksiTang(2)
+    bounds = np.tile((-5., 5.), (2, 1))
+    hyperdrive(stybtang, bounds, args.results_path, n_iterations=10)
+
+
+if __name__=='__main__':
+    main()
--- hyperspace/space/robo/__init__.py
+++ hyperspace/space/robo/__init__.py
@@ -1,9 +1,11 @@
 from .space import RoboInteger
 from .space import RoboReal
 from .mapping_space import create_robospace
+from .mapping_space import convert_robospace
 
 
 __all__ = (
+    "convert_robospace",
     "create_robospace",
     "RoboInteger",
     "RoboReal"
