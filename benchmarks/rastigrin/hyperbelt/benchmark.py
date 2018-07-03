9107b3ba5b02f5a2fa6530771497799fd839fe5f
yngtodd <young.todd.mk@gmail.com>
2018-07-02 21:39:59 -0400

    Adding benchmark for Rastigrin

---
+++ benchmarks/rastigrin/hyperbelt/benchmark.py
@@ -0,0 +1,31 @@
+import argparse
+import numpy as np
+
+from hyperspace import hyperbelt 
+from hyperspace.benchmarks import Rastigrin 
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Setup experiment.')
+    parser.add_argument('--ndims', type=int, help='Number of dimensions for Styblinski-Tang function')
+    parser.add_argument('--results_dir', type=str, help='Path to results directory.')
+    args = parser.parse_args()
+
+    rastigrin = Rastigrin(args.ndims)
+    bounds = np.tile((-5.12, 5.12), (args.ndims, 1))
+
+    hyperbelt(objective=rastigrin,
+              hyperparameters=bounds,
+              results_path=args.results_dir,
+              model="RAND",
+              n_iterations=50,
+              sampler='lhs',
+              n_samples=2,
+              model_verbose=False,
+              hyperband_verbose=True,
+              random_state=0)
+
+
+if __name__ == '__main__':
+    main()
+
