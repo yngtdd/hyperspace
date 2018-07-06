f856a4e4037538f8f431cb1058d3ac5f8e7a3c72
Todd <myoung44@vols.utk.edu>
2018-07-06 15:18:45 -0400

    Updating setupy.py

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(
     name='hyperspace',
     version="0.2.0",
     packages=find_packages(),
-    install_requires=['numpy', 'scikit-optimize', 'scikit-learn', 'mpi4py'],
+    install_requires=['numpy', 'scikit-optimize', 'scikit-learn', 'pyrfr', 'mpi4py'],
+    dependency_links = [
+      'https://github.com/automl/RoBO/tarball/master',
+    ],
 
     # metadata for upload to PyPI
     author="Todd Young",
