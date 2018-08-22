1b7571e5d8c32280f8bcd37e0929e56a3912d5e9
Todd <myoung44@vols.utk.edu>
2018-08-22 12:03:45 -0500

    Moving to 2.2.3 for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hyperspaces',
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     install_requires=['scikit-optimize', 'scikit-learn', 'mpi4py'],
     author="Todd Young",
