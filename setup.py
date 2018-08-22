0794ca913a9eba95b4ad4d906a53da212a462dd2
Todd <myoung44@vols.utk.edu>
2018-08-22 11:52:23 -0500

    Removing optional dependence. RoBO is difficult outside of Ubuntu

--- setup.py
+++ setup.py
@@ -6,11 +6,6 @@ setup(
     version="0.2.1",
     packages=find_packages(),
     install_requires=['scikit-optimize', 'scikit-learn', 'mpi4py'],
-    dependency_links = [
-      'https://github.com/automl/RoBO/tarball/master',
-    ],
-
-    # metadata for upload to PyPI
     author="Todd Young",
     author_email="youngmt1@ornl.gov",
     description="Distributed Bayesian model-based optimization",
