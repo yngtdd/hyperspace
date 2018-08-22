a1176a2b84f3808a63c1bc6b416a42e5932cbeed
Todd <myoung44@vols.utk.edu>
2018-08-22 11:53:28 -0500

    Updating minor version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hyperspaces',
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=['scikit-optimize', 'scikit-learn', 'mpi4py'],
     author="Todd Young",
