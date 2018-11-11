7f2e44254bbd365e27204599a869d6f9f6bd2106
yngtodd <young.todd.mk@gmail.com>
2018-11-11 10:17:54 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='hyperspaces',
     version="0.2.3",
     packages=find_packages(),
-    install_requires=['mpi4py', 'cython', 'sphinxcontrib-bibtex'],
+    install_requires=['mpi4py', 'cython'],
     author="Todd Young",
     author_email="youngmt1@ornl.gov",
     description="Distributed Bayesian model-based optimization",
