edbff3f8976006ab20a472486db55fe728ebff96
yngtodd <young.todd.mk@gmail.com>
2019-01-15 10:58:28 -0500

    Update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../../hyperspace'))
 
 import sphinx_rtd_theme
+import hyperspace
 
 # -- General configuration ------------------------------------------------
 
